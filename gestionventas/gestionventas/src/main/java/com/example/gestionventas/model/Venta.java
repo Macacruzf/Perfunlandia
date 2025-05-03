76627a958ec248f555d85c1d146aa98bb410aefb
Macacruzf <maca.cruz@duocuc.cl>
2025-05-03 19:23:48 -0400

    Add files via upload

---
+++ gestionventas/gestionventas/src/main/java/com/example/gestionventas/model/Venta.java
@@ -0,0 +1,39 @@
+package com.example.gestionventas.model;
+
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "venta")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Venta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Date fecha;
+
+    @Column(nullable = false)
+    private Integer montoTotal;
+
+    @Column(nullable = false, length = 12)
+    private String runCliente;
+
+    @Column(length = 200)
+    private String detalles;
+
+}
---
+++ gestionventas/gestionventas/src/test/java/com/example/gestionventas/GestionventasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionventasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
