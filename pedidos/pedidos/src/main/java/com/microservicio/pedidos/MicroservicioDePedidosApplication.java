a81a6daa0c01d9649acc94545d84688c92ca5d59
CesarHenriquez <ce.henriquezn@duocuc.cl>
2025-05-05 17:06:27 -0400

    Microservicio Pedidos
    Primera fase Microservicio de pedidos

---
+++ pedidos/pedidos/src/main/java/com/microservicio/pedidos/MicroservicioDePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioDePedidosApplication.class, args);
+	}
+
+}
---
+++ pedidos/pedidos/src/main/java/com/microservicio/pedidos/model/DetallePedido.java
@@ -0,0 +1,38 @@
+package com.microservicio.pedidos.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "detallePedido")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DetallePedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id", nullable = false)
+    private Pedido pedido;
+
+    @Column(nullable = false)
+    private Long productoId; // ID del producto (relación indirecta con microservicio de productos)
+
+    @Column(nullable = false)
+    private Integer cantidad;
+
+    @Column(nullable = false)
+    private Integer precioUnitario;
+
+}
