b5898cd414c46296da19edd0ab68c0f24f479a8e
uFeiku <die.mirandab@duocuc.cl>
2025-05-17 20:50:26 -0400

    inventario finished

--- inventarios/src/main/java/com/example/inventarios/service/InventarioService.java
+++ inventarios/src/main/java/com/example/inventarios/service/InventarioService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.example.inventarios.model.Inventario;
 import com.example.inventarios.repository.InventarioRepository;
 
+import jakarta.transaction.Transactional;
+
 
 
 
@@ -22,10 +24,28 @@ public class InventarioService {
         return inventarioRepository.findById(idInventario);
     }
 
-    
-    
+    @Transactional //para actualizar el stock (para el controller)
+    public Inventario actualizarStock(Long idProducto, Long idSucursal, Integer cantidad) {
+        
+        Inventario inventario = inventarioRepository
+            .findByIdProductoAndSucursalId(idProducto, idSucursal)
+            .orElse(new Inventario(idProducto, idSucursal, null, cantidad));
 
-       
+        //actualiza
+        inventario.setCantidad(inventario.getCantidad() + cantidad);
+
+        //guarda
+        return inventarioRepository.save(inventario);
+    }
+    
+    public Inventario getInventarioPorId(Long id){
+        return inventarioRepository.findById(id)
+        .orElseThrow(()-> new RuntimeException("Inventario No encontrado"));
+    }
+
+    public List<Inventario> listarTodo() {
+        return inventarioRepository.findAll();
+    }
 
 }
 
