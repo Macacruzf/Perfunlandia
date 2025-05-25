c9576cf24011dca24b0792d198cdfff1f5f43c60
Macacruzf <maca.cruz@duocuc.cl>
2025-05-24 22:52:05 -0400

    Delete catalogo/catalogo directory
    se va agrega una actualizacion

--- catalogo/catalogo/src/main/java/com/catalogo/repository/CategoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.catalogo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.catalogo.model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
-}
