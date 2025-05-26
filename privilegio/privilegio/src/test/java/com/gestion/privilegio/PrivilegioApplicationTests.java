7d2bf387e54266b8ce54ec605f4543b666686ee5
CesarHenriquez <ce.henriquezn@duocuc.cl>
2025-05-26 13:16:13 -0400

    Microservicio Privilegio
    En este microservicio mediante un usuario ya registrado y validado. Dependiendo de su rol, se le muestra un mensaje de que es lo que puede hacer y que no puede hacer. Versión final

---
+++ privilegio/privilegio/src/test/java/com/gestion/privilegio/PrivilegioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.privilegio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivilegioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
